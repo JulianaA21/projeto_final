a1b00e7a02019027727a7150d72a9d2103a23d3e
JulianaA21 <seremjuliana@gmail.com>
2025-06-03 12:33:48 +0100

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 // Criar a conexão com o MySQL
-const db = mysql.createConnection({
+const db= mysql.createConnection({
   host: 'localhost',   // Coloca aqui o host correto
   user: 'root',        // Coloca o teu nome de utilizador do MySQL
   password: '123',        // Coloca a tua password do MySQL
-  database: 'auth_demo' // Base de dados que usas
+  database: 'Padel_Coimbra' // Nome Base de dados 
 });
 
 // Testa a conexão
@@ -16,6 +16,19 @@ db.connect(err => {
   }
   console.log('Conectado ao MySQL');
 });
+
+//try {
+  //const [results, fields] = await connection.query(
+ //   'SELECT * FROM `padel_coimbra`'
+ // );
+
+ // console.log(results); // results contains rows returned by server
+ // console.log(fields); // fields contains extra meta data about results, if available
+//} catch (err) {
+//console.log(err);
+//}
+
+
 
 // Exporta a instância de db
 module.exports = db;
